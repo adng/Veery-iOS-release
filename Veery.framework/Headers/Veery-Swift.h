526368072cba3a621b83ece65bcfcd4f3857486a
malekhassani <malek.1905@live.fr>
2018-01-09 14:40:15 +0100

    Version 1.1.2

--- Veery.framework/Headers/Veery-Swift.h
+++ Veery.framework/Headers/Veery-Swift.h
@@ -274,7 +274,7 @@ SWIFT_CLASS_PROPERTY(@property (nonatomic, class, readonly) NSInteger DEACTIVATE
 - (void)getAlllocationTodebug;
 - (void)requestPoiUpdate;
 - (void)stopPoiUpdate;
-- (NSString * _Nullable)getStatus SWIFT_WARN_UNUSED_RESULT;
+- (NSString * _Nonnull)getStatus SWIFT_WARN_UNUSED_RESULT;
 @end
 
 
